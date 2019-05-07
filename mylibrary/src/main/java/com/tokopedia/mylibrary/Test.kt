bb5ee44e49ae07b30aa1f5c0902bbb50e0c288b3
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-05-07 15:33:30 +0700

    Initial commit

---
+++ mylibrary/src/main/java/com/tokopedia/mylibrary/Test.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.mylibrary
+
+import android.util.Log
+
+/**
+ * Created by hendry on 2019-05-06.
+ */
+object Test{
+    fun test(){
+        Log.i("Test", "test")
+    }
+}
---
+++ mylibrary/src/main/java/com/tokopedia/mylibrary/TestActivity.java
@@ -0,0 +1,15 @@
+package com.tokopedia.mylibrary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hendry on 2019-05-07.
+ */
+public class TestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_2);
+    }
+}
