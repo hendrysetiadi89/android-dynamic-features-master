4e9c31461c7c77cb4e5e0e8fb5a897857a4d850c
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-05-07 18:38:33 +0700

    clean some code

--- mylibrary/src/main/java/com/tokopedia/mylibrary/TestActivity.java
+++ mylibrary/src/main/java/com/tokopedia/mylibrary/TestActivity.java
@@ -1,7 +1,10 @@
 package com.tokopedia.mylibrary;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+
+import com.google.android.play.core.splitcompat.SplitCompat;
 
 /**
  * Created by hendry on 2019-05-07.
@@ -12,4 +15,11 @@ public class TestActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_2);
     }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+        SplitCompat.install(this);
+    }
+
 }
